5debf2c5bd98642f9708c733f236b1215e942c0b
Dennis Christman <dennis.christman@gmail.com>
2017-02-27 11:11:05 -0500

    Created caeser cipher program. Worked on the first try, minus some typos!

---
+++ README.md
@@ -0,0 +1,3 @@
+This program was created in Ruby following the Odin Project curriculum.
+
+caeser_cipher is a simple program to replicate the Caeser Cipher, where a message is passed along with a shift key, and an ecnrypted version is returned by shifting each letter forward in the alphabet according to the key. Capitalization should remain the same, and punctuation should be ignored.
---
+++ caeser.rb
@@ -0,0 +1,31 @@
+#Get the message to be encoded and the key
+puts "Enter your message"
+msg = gets.chomp
+key = 0
+output = ""
+alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+while (key < 1 || key > 25)
+  puts "Enter a shift key between 1 and 25"
+  key = gets.chomp.to_i
+  puts "invalid key, please try again" if (key < 1 && key > 25)
+end
+
+letters = msg.split(//)
+letters.each do |letter|
+  if alphabet.include?(letter.downcase)
+    if letter.downcase == letter
+      letter_is_uppercase = false
+    else
+      letter_is_uppercase = true
+    end
+    letter = letter.downcase
+    position = alphabet.index(letter)
+    position += key
+    position -+ 26 if position > 25
+    letter = alphabet[position]
+    letter = letter.upcase if letter_is_uppercase
+  end
+  output += letter
+end
+puts output
